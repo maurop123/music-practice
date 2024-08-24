432dbfa9c7411c474f27b238e7f5e4a4477b8e4b
Mauro P <maurop123@gmail.com>
2024-08-23 21:00:25 -0600

    add build steps

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/music-practice',
   plugins: [
     vue(),
     vueDevTools(),
